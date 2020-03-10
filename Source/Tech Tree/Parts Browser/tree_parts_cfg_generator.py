c4233975861d62540ac86edbeb86e9a67cf51fb8
nepphhh <kikjezrous@gmail.com>
2020-03-10 14:45:38 -0400

    Made the config generator fail more graciously

--- Source/Tech Tree/Parts Browser/tree_parts_cfg_generator.py
+++ Source/Tech Tree/Parts Browser/tree_parts_cfg_generator.py
@@ -41,12 +41,15 @@ def generate_parts_tree(parts, module_tags_with_prefixes):
     text_file.close()
         
 def generate_part_config(part, module_tags_with_prefixes):
-    module_tags = ''
-    for module_tag in part['module_tags']:
-        # some module tags have a prefix (usually 'Tag') so if it has a prefix, prefix it.
-        prefix = module_tags_with_prefixes[module_tag].strip()
-        module_tags += module_template.substitute(module_tag=prefix + module_tag)
-    if len(module_tags) > 0:
-        module_tags = "\n" + module_tags + "\n"
-    return module_part_config_template.substitute(name=part['name'], mod=part['mod'], technology=part['technology'], cost=part['cost'], entry_cost=part['entry_cost'], rp0_conf=str(part['rp0_conf']).lower(), is_non_proc=str(part['is_non_proc']).lower(), module_tags=module_tags)
-
+    try:
+        module_tags = ''
+        for module_tag in part['module_tags']:
+            # some module tags have a prefix (usually 'Tag') so if it has a prefix, prefix it.
+            prefix = module_tags_with_prefixes[module_tag].strip()
+            module_tags += module_template.substitute(module_tag=prefix + module_tag)
+        if len(module_tags) > 0:
+            module_tags = "\n" + module_tags + "\n"
+        return module_part_config_template.substitute(name=part['name'], mod=part['mod'], technology=part['technology'], cost=part['cost'], entry_cost=part['entry_cost'], rp0_conf=str(part['rp0_conf']).lower(), is_non_proc=str(part['is_non_proc']).lower(), module_tags=module_tags)
+    except:
+        print("Missing fields for .json of part named", part['name'], "! Aborting config generation.")
+        exit()
